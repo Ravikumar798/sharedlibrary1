fbeaec16a434dbe281bc791c279bcc7886c8e513
Ravikumar798 <127723506+Ravikumar798@users.noreply.github.com>
2024-08-30 16:15:07 +0530

    math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,4 +1,13 @@
 def add(a,b)
 {
   echo "Adding of ${a} and ${b} is ${a+b}"
-}  
+}
+def sub(a,b)
+{
+  echo "subracting of ${a} and ${b} is ${a-b}"
+}
+def mul(a,b)
+{
+  echo "multiplying of ${a} and ${b} is ${a*b}"
+}
+  
