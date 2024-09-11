5945008b702d9a0896c7b9ee2979dd5d535b3f07
Ravikumar798 <127723506+Ravikumar798@users.noreply.github.com>
2024-09-11 11:40:10 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,1 +1,12 @@
-
+def contDownload(repo)
+{
+  echo git "https://github.com/intelliqittrainings/${repo}.git"
+}
+def contBuild()
+{
+   echo sh "mvn package"
+}
+def deploytomcat(ip,context)
+{
+  echo deploy adapters:"[tomcat9(credentialsId: 'ee329ab3-6634-416e-9558-5e974dec7e8c', path: '', url: 'http://${ip}:8080')], contextPath: '${context}', war: '**/*.war"
+}
