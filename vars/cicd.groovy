c150d9cf8c30a4e03cd84248bc8416af57e26a80
Ravikumar798 <127723506+Ravikumar798@users.noreply.github.com>
2024-09-11 12:26:58 +0530

    cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,12 +1,12 @@
 def contDownload(repo)
 {
-  echo git "https://github.com/Ravikumar798/${repo}.git"
+  echo "git https://github.com/Ravikumar798/${repo}.git"
 }
 def contBuild()
 {
-   echo sh "mvn package"
+   echo "sh mvn package"
 }
 def deploytomcat(ip,context)
 {
-  echo deploy "adapters: [tomcat9(credentialsId: 'ee329ab3-6634-416e-9558-5e974dec7e8c', path: '', url: 'http://${ip}:8080')], contextPath: '${context}', war: '**/*.war'"
+  echo "deploy adapters: [tomcat9(credentialsId: 'ee329ab3-6634-416e-9558-5e974dec7e8c', path: '', url: 'http://${ip}:8080')], contextPath: '${context}', war: '**/*.war'"
 }
